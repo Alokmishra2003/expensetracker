02713c809ff0b7b40e2b47fa27d9c2833d862b09
Alok Mishra <99598676+Alokmishra2003@users.noreply.github.com>
2023-08-15 19:24:18 +0530

    Add files via upload

---
+++ expenses.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:small_app/newexpense.dart';
+import 'package:small_app/expenseslist.dart';
+import 'package:small_app/expense.dart';
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:small_app/expenses.dart'; // Add a semicolon at the end of this line
+
+void main() {
+  
+  runApp(
+    const MaterialApp(
+      
+      home: Expenses(),
+    ),
+  );
+}
